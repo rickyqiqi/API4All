707525439988d61a19805862ef63ae803b332d30
ricky <ricky.gong@szlegate.com>
2016-10-13 11:51:36 +0800

    1. 去除了黑名单股票过滤； 2. 增加了大盘价格止损调整倍率、个股止损调整倍率和个股止盈调整倍率； 3. 增加了二八涨幅定期输出代码。

--- 二八小市值择时买卖.py
+++ 二八小市值择时买卖.py
@@ -7,7 +7,7 @@
 止损策略：
 
     大盘止损：(可选)
-        1. 每分钟取大盘前130日的最低价和最高价，如果最高大于最低的两倍则清仓，停止交易。
+        1. 每分钟取大盘前160日的最低价和最高价，如果最高大于最低的两倍则清仓，停止交易。
         2. 每分钟判断大盘是否呈现三只黑鸦止损，如果是则当天清仓并停止交易，第二天停止交
            易一天。
 
@@ -128,7 +128,7 @@ def set_param():
     # 配置是否过滤创业板股票
     g.filter_gem = True
     # 配置是否过滤黑名单股票，回测建议关闭，模拟运行时开启
-    g.filter_blacklist = True
+    g.filter_blacklist = False
 
     # 是否对股票评分
     g.is_rank_stock = True
@@ -151,12 +151,14 @@ def set_param():
     g.index_growth_rate_20 = 0.01
 
     # 配置是否根据大盘历史价格止损
-    # 大盘指数前130日内最高价超过最低价2倍，则清仓止损
+    # 大盘指数前160日内最高价超过最低价2倍，则清仓止损
     # 注：关闭此止损，收益增加，但回撤会增加
     g.is_market_stop_loss_by_price = True
     if g.is_market_stop_loss_by_price:
         # 配置价格止损判定指数，默认为上证指数，可修改为其他指数
         g.index_4_stop_loss_by_price = '000001.XSHG'
+        # 调整倍数
+        g.ajust_rate_4_stop_loss_by_price = 1.10
 
     # 配置三黑鸦判定指数，默认为上证指数，可修改为其他指数
     g.index_4_stop_loss_by_3_black_crows = '000001.XSHG'
@@ -169,9 +171,13 @@ def set_param():
         g.dst_drop_minute_count = 10
 
     # 配置是否个股止损
-    g.is_stock_stop_loss =False
+    g.is_stock_stop_loss = True
+    if g.is_stock_stop_loss:
+        g.ajust_rate_4_stock_stop_loss = 1.00
     # 配置是否个股止盈
-    g.is_stock_stop_profit = False
+    g.is_stock_stop_profit = True
+    if g.is_stock_stop_profit:
+        g.ajust_rate_4_stock_stop_profit = 1.10
     
 def log_param():
     log.info("调仓日频率: %d日" %(g.period))
@@ -206,6 +212,7 @@ def log_param():
     log.info("是否开启大盘历史高低价格止损: %s" %(g.is_market_stop_loss_by_price))
     if g.is_market_stop_loss_by_price:
         log.info("大盘价格止损判定指数: %s - %s" %(g.index_4_stop_loss_by_price, get_security_info(g.index_4_stop_loss_by_price).display_name))
+        log.info("大盘价格止损调整倍率: %s" %(g.ajust_rate_4_stop_loss_by_price))
 
     log.info("大盘三黑鸦止损判定指数: %s - %s" %(g.index_4_stop_loss_by_3_black_crows, get_security_info(g.index_4_stop_loss_by_3_black_crows).display_name))
     log.info("是否开启大盘三黑鸦止损: %s" %(g.is_market_stop_loss_by_3_black_crows))
@@ -213,7 +220,11 @@ def log_param():
         log.info("三黑鸦止损开启需要当日大盘为跌的分钟计数达到: %d" %(g.dst_drop_minute_count))
 
     log.info("是否开启个股止损: %s" %(g.is_stock_stop_loss))
+    if g.is_stock_stop_loss:
+        log.info("个股止损调整倍率: %s" %(g.ajust_rate_4_stock_stop_loss))
     log.info("是否开启个股止盈: %s" %(g.is_stock_stop_profit))
+    if g.is_stock_stop_profit:
+        log.info("个股止盈调整倍率: %s" %(g.ajust_rate_4_stock_stop_profit))
 
 # 重置当日参数，仅针对需要当日需要重置的参数
 def reset_day_param():
@@ -250,42 +261,47 @@ def handle_data(context, data):
     hour = context.current_dt.hour
     minute = context.current_dt.minute
     
-    # 每天下午14:50调仓
-    if hour == g.adjust_position_hour and minute == g.adjust_position_minute:
-        do_handle_data(context, data)
+    if ((hour >= 9 and hour <= 11) and (minute == 30)) or ((hour >= 13 and hour <= 15) and (minute == 0)) or (hour == g.adjust_position_hour and minute == g.adjust_position_minute):
+        # 调仓换股标记
+        changeposition = False
+        # 每天下午14:50调仓
+        if hour == g.adjust_position_hour and minute == g.adjust_position_minute:
+            changeposition = True
+        do_handle_data(context, data, changeposition)
 
-def do_handle_data(context, data):
-    log.info("调仓日计数 [%d]" %(g.day_count))
-    
+def do_handle_data(context, data, changeposition=True):
     # 回看指数前20天的涨幅
     gr_index2 = get_growth_rate(g.index2)
     gr_index8 = get_growth_rate(g.index8)
     log.info("当前%s指数的20日涨幅 [%.2f%%]" %(get_security_info(g.index2).display_name, gr_index2*100))
     log.info("当前%s指数的20日涨幅 [%.2f%%]" %(get_security_info(g.index8).display_name, gr_index8*100))
 
-    if gr_index2 <= g.index_growth_rate_20 and gr_index8 <= g.index_growth_rate_20:
-        clear_position(context)
-        g.day_count = 0
-    else: #if  gr_index2 > g.index_growth_rate_20 or ret_index8 > g.index_growth_rate_20:
-        if g.day_count % g.period == 0:
-            log.info("==> 满足条件进行调仓")
-            buy_stocks = pick_stocks(context, data)
-            log.info("选股后可买股票: %s" %(buy_stocks))
-            adjust_position(context, buy_stocks)
-        g.day_count += 1
+    if changeposition :
+        log.info("调仓日计数 [%d]" %(g.day_count))
+
+        if gr_index2 <= g.index_growth_rate_20 and gr_index8 <= g.index_growth_rate_20:
+            clear_position(context)
+            g.day_count = 0
+        else: #if  gr_index2 > g.index_growth_rate_20 or ret_index8 > g.index_growth_rate_20:
+            if g.day_count % g.period == 0:
+                log.info("==> 满足条件进行调仓")
+                buy_stocks = pick_stocks(context, data)
+                log.info("选股后可买股票: %s" %(buy_stocks))
+                adjust_position(context, buy_stocks)
+            g.day_count += 1
 
 def market_stop_loss_by_price(context, index):
-    # 大盘指数前130日内最高价超过最低价2倍，则清仓止损
+    # 大盘指数前160日内最高价超过最低价2倍，则清仓止损
     # 基于历史数据判定，因此若状态满足，则当天都不会变化
     # 增加此止损，回撤降低，收益降低
 
     if not g.is_day_stop_loss_by_price:
         h = attribute_history(index, 160, unit='1d', fields=('close', 'high', 'low'), skip_paused=True)
-        low_price_130 = h.low.min()
-        high_price_130 = h.high.max()
-        if high_price_130 > 2.2 * low_price_130 and h['close'][-1]<h['close'][-4]*1 and  h['close'][-1]> h['close'][-100]:
+        low_price_160 = h.low.min()
+        high_price_160 = h.high.max()
+        if high_price_160 > 2 * g.ajust_rate_4_stock_stop_profit * low_price_160 and h['close'][-1]<h['close'][-4]*1 and  h['close'][-1]> h['close'][-100]:
             # 当日第一次输出日志
-            log.info("==> 大盘止损，%s指数前130日内最高价超过最低价2倍, 最高价: %f, 最低价: %f" %(get_security_info(index).display_name, high_price_130, low_price_130))
+            log.info("==> 大盘止损，%s指数前160日内最高价超过最低价2倍, 最高价: %f, 最低价: %f" %(get_security_info(index).display_name, high_price_160, low_price_160))
             g.is_day_stop_loss_by_price = True
 
     if g.is_day_stop_loss_by_price:
@@ -380,7 +396,7 @@ def stock_stop_loss(context, data):
             
         threshold = get_stop_loss_threshold(stock, g.period)
         #log.debug("个股止损阈值, stock: %s, threshold: %f" %(stock, threshold))
-        if cur_price < g.last_high[stock] * (1 - threshold):
+        if cur_price < g.last_high[stock] * (1 - threshold) * g.ajust_rate_4_stock_stop_loss:
             log.info("==> 个股止损, stock: %s, cur_price: %f, last_high: %f, threshold: %f" 
                 %(stock, cur_price, g.last_high[stock], threshold))
 
@@ -395,7 +411,7 @@ def stock_stop_profit(context, data):
         cur_price = data[stock].close
         threshold = get_stop_profit_threshold(stock, g.period)
         #log.debug("个股止盈阈值, stock: %s, threshold: %f" %(stock, threshold))
-        if cur_price > position.avg_cost * (1 + threshold):
+        if cur_price > position.avg_cost * (1 + threshold) * g.ajust_rate_4_stock_stop_profit:
             log.info("==> 个股止盈, stock: %s, cur_price: %f, avg_cost: %f, threshold: %f" 
                 %(stock, cur_price, g.last_high[stock], threshold))
 
