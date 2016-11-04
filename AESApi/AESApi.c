717982a6cdd64eb563c2ef82e058278ef0ce575e
ricky <ricky.gong@szlegate.com>
2016-11-04 17:22:09 +0800

    1. 增加了AES接口文件。

---
+++ AESApi/AESApi.c
@@ -0,0 +1,79 @@
+/*****************************************************************************/
+/*                                                                           */
+/*    Copyright (C) -                              - All rights reserved     */
+/*                                                                           */
+/*****************************************************************************/
+/*                                                                           */
+/*  Except if expressly provided in a dedicated License Agreement, you are   */
+/*  not authorized to:                                                       */
+/*                                                                           */
+/*  1. Use, copy, modify or transfer this software component, module or      */
+/*  product, including any accompanying electronic or paper documentation    */
+/*  (together, the "Software").                                              */
+/*                                                                           */
+/*  2. Remove any product identification, copyright, proprietary notices or  */
+/*  labels from the Software.                                                */
+/*                                                                           */
+/*  3. Modify, reverse engineer, decompile, disassemble or otherwise attempt */
+/*  to reconstruct or discover the source code, or any parts of it, from the */
+/*  binaries of the Software.                                                */
+/*                                                                           */
+/*  4. Create derivative works based on the Software (e.g. incorporating the */
+/*  Software in another software or commercial product or service without a  */
+/*  proper license).                                                         */
+/*                                                                           */
+/*  By installing or using the "Software", you confirm your acceptance of the*/
+/*  hereabove terms and conditions.                                          */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*  History:                                                                 */
+/*****************************************************************************/
+/*  Date       * Author          * Changes                                   */
+/*****************************************************************************/
+/*  2016-11-03 * Ricky Gong      * Creation of the file                      */
+/*             *                 *                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Include Files                                                            */
+/*                                                                           */
+/*****************************************************************************/
+#include <stdio.h>
+#include "AESApi.h"
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Definitions                                                              */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Variable Declarations                                                    */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Function Declarations                                                    */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Function Implementations                                                 */
+/*                                                                           */
+/*****************************************************************************/
+void point_print(struct point * p)
+{
+    printf("x = %d, y = %d\n", p->x, p->y);
+}
---
+++ AESApi/AESApi.h
@@ -0,0 +1,80 @@
+#ifndef _AESAPI_H_
+#define _AESAPI_H_
+
+/*****************************************************************************/
+/*                                                                           */
+/*    Copyright (C) -                              - All rights reserved     */
+/*                                                                           */
+/*****************************************************************************/
+/*                                                                           */
+/*  Except if expressly provided in a dedicated License Agreement, you are   */
+/*  not authorized to:                                                       */
+/*                                                                           */
+/*  1. Use, copy, modify or transfer this software component, module or      */
+/*  product, including any accompanying electronic or paper documentation    */
+/*  (together, the "Software").                                              */
+/*                                                                           */
+/*  2. Remove any product identification, copyright, proprietary notices or  */
+/*  labels from the Software.                                                */
+/*                                                                           */
+/*  3. Modify, reverse engineer, decompile, disassemble or otherwise attempt */
+/*  to reconstruct or discover the source code, or any parts of it, from the */
+/*  binaries of the Software.                                                */
+/*                                                                           */
+/*  4. Create derivative works based on the Software (e.g. incorporating the */
+/*  Software in another software or commercial product or service without a  */
+/*  proper license).                                                         */
+/*                                                                           */
+/*  By installing or using the "Software", you confirm your acceptance of the*/
+/*  hereabove terms and conditions.                                          */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*  History:                                                                 */
+/*****************************************************************************/
+/*  Date       * Author          * Changes                                   */
+/*****************************************************************************/
+/*  2016-11-03 * Ricky Gong      * Creation of the file                      */
+/*             *                 *                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Include Files                                                            */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Definitions                                                              */
+/*                                                                           */
+/*****************************************************************************/
+struct point
+{
+    int x;
+    int y;
+};
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Variable Declarations                                                    */
+/*                                                                           */
+/*****************************************************************************/
+
+
+/*****************************************************************************/
+/*                                                                           */
+/*  Function Declarations                                                    */
+/*                                                                           */
+/*****************************************************************************/                        
+void point_print(struct point * p);
+
+
+#endif  // _AESAPI_H_
+
+
